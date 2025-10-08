8f7c79b000129140149327bfd7fe927a86cfcdff
SahidXDD <sahidtoapanta4@gmail.com>
2025-10-07 19:25:49 -0500

    el botón modifica una imagen

--- ejercicioGit/ejercicio.js
+++ ejercicioGit/ejercicio.js
@@ -1,3 +1,9 @@
 saludar = function(){
+    let cmpImagenSaludo;
+    cmpImagenSaludo=document.getElementById("imagenSaludo")
+    cmpImagenSaludo.src="./imagenes/ola.gif"
     console.log("Ingresa a saludar");
+    
+
+
 }
