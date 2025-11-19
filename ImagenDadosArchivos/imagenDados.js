d6f0c2599b1c4c045389aa2e48ac13484a66b267
SahidXDD <sahidtoapanta4@gmail.com>
2025-11-19 14:46:52 -0500

    ya pasó

---
+++ ImagenDadosArchivos/imagenDados.js
@@ -0,0 +1,76 @@
+let puntos=0;
+
+let lanzamientos=5;
+jugar=function(){
+    let resultado;
+    resultado=lanzarDado();
+    console.log(resultado);
+    mostrarCara(resultado);
+    modificarPuntos(resultado)
+    modificarLanzamientos()
+   
+    
+    
+}
+
+modificarPuntos = function(numero){
+    puntos=puntos+numero;
+    cambiarTexto("lblPuntos",puntos);
+    if(puntos>19){
+        cambiarTexto("txtGanador","GANASTE");
+       }else if(puntos>=20 ){
+            limpiar()
+    
+          
+}
+}
+modificarLanzamientos = function(){
+    lanzamientos=lanzamientos-1;
+    cambiarTexto("lblLanzamientos",lanzamientos);
+    if(lanzamientos==0){
+        cambiarTexto("txtPerdedor","GAME OVER");
+            }else if(lanzamientos<0,puntos>=20 ){
+                limpiar()
+
+}
+}
+
+
+limpiar = function(){
+    
+    cambiarTexto("lblPuntos","0")
+    cambiarTexto("lblLanzamientos","5")
+    cambiarTexto("txtGanador","")
+    cambiarTexto("txtPerdedor","")
+    puntos=0;
+    lanzamientos=5;
+
+}
+
+mostrarCara = function(numero){
+if(numero == 1){
+    cambiarImagen("imgDado","dados1.png");
+}else if(numero == 2){
+    cambiarImagen("imgDado","dados2.png");
+}else if(numero == 3){
+    cambiarImagen("imgDado","dados3.png");
+}else if(numero == 4){
+    cambiarImagen("imgDado","dados4.png");
+}else if(numero == 5){
+    cambiarImagen("imgDado","dados5.png");
+}else if(numero == 6){
+    cambiarImagen("imgDado","dados6.png");
+}
+}
+
+lanzarDado=function(){
+    let aleatorio;
+    let aleatorioMultiplicado;
+    let aleatorioEntero;
+    let valorDado;
+    aleatorio=Math.random();
+    aleatorioMultiplicado=aleatorio*6;
+    aleatorioEntero=parseInt(aleatorioMultiplicado);
+    valorDado=aleatorioEntero+1;
+    return valorDado;
+}
